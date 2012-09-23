474fa7500d265e3eebd0c8efa52b6955ea0147e7
Ron Shapiro <shapiro.rd@gmail.com>
2012-09-23 12:53:03 -0400

    declare a matrix with an int array

--- Matrix.java
+++ Matrix.java
@@ -11,6 +11,13 @@ public class Matrix{
       for (int j = 0; j < array[0].length; j++)
         mMatrix[i][j] = array[i][j];
   }
+
+  public Matrix(int[][] array){
+    mMatrix = new double[array.length][array[0].length];
+    for (int i = 0; i < array.length; i++)
+      for (int j = 0; j < array[0].length; j++)
+        mMatrix[i][j] = (double) array[i][j];
+  }
   
   public Matrix clone(){
     /* TODO create a new matrix with the same values*/
