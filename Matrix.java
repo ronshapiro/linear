813d64e4045bd212352c22f984cbe3f35a323b48
Ron Shapiro <shapiro.rd@gmail.com>
2012-09-23 03:27:10 -0400

    Tester and removed .class from repo

--- Matrix.java
+++ Matrix.java
@@ -5,6 +5,7 @@ public class Matrix{
     mMatrix = new double[n][m];
   }
   public Matrix(double[][] array){
+    //assert that all inner arrays are same length
     mMatrix = new double[array.length][array[0].length];
     for (int i = 0; i < array.length; i++)
       for (int j = 0; j < array[0].length; j++)
---
+++ MatrixPowerTester.java
@@ -0,0 +1,7 @@
+public class MatrixPowerTester{
+  public static void main(String[] args){
+    for (int i = 0; i < args.length; i++) System.out.println(args[i]);
+    Matrix m = new Matrix(1,1);
+  }
+}
+  
