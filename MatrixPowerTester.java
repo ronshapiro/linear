fb9297f42bba2e7b6962db2f2409fa7f1f0c4c11
Ron Shapiro <shapiro.rd@gmail.com>
2012-09-23 03:30:46 -0400

    Notes

--- MatrixPowerTester.java
+++ MatrixPowerTester.java
@@ -2,6 +2,18 @@ public class MatrixPowerTester{
   public static void main(String[] args){
     for (int i = 0; i < args.length; i++) System.out.println(args[i]);
     Matrix m = new Matrix(1,1);
+    /*
+    Note cloning might not even be necessary - 
+    suppose k = 4
+    a a a a a a a a
+    compute a*a once, then duplicate matrix 3 more times
+    b = a * a;
+    b b.clone() b.clone() b.clone()
+    compute b*b once, then duplicate matrix 1 more time
+    c = b * b
+    c c.clone()
+    compute d = c * c
+    */
   }
 }
   
