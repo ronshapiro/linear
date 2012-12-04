245ac92ea36f6fcc0fc9451b9d6b0f681ac2efeb
Ron Shapiro <shapiro.rd@gmail.com>
2012-12-04 09:52:37 -0500

    Removed old Tester code

--- MatrixTester.java
+++ MatrixTester.java
@@ -12,8 +12,7 @@ public class MatrixTester{
       System.exit(0);
     }
 
-    //Matrix A1 = new Matrix(new int[][]{{2,-1,0},{-1,2,-1},{0,-1,2}});
-    Matrix A1 = new Matrix(new int[][]{
+    Matrix A = new Matrix(new int[][]{
         {2,-1,0,0,0,0,0,0,0,0},
         {-1,2,-1,0,0,0,0,0,0,0},
         {0,-1,2,-1,0,0,0,0,0,0},
@@ -25,51 +24,6 @@ public class MatrixTester{
         {0,0,0,0,0,0,0,-1,2,-1},
         {0,0,0,0,0,0,0,0,-1,2},
       });
-    System.out.println(A1.inverseIteration());
-    System.exit(0);
-
-    Scanner inputFile = null;
-    try {
-      inputFile = new Scanner(new File(args[0]));
-    } catch (FileNotFoundException e){
-      System.err.println("IO Error: Input file not found");
-      System.exit(0);
-    }
-
-    int m = -1;
-    double C = -1, D = -1, E = 1;
-    for(int i = 0; inputFile.hasNextLine(); i++){
-      String line = inputFile.nextLine();
-      if (i == 0){
-        m = Integer.parseInt(line);        
-      }
-      else if (i == 1){
-        C = Double.parseDouble(line);
-      }
-      else if (i == 2){
-        D = Double.parseDouble(line);
-      }
-      else if (i == 3){
-        E = Double.parseDouble(line);
-      }
-    }
-
-    Matrix A = new Matrix(m,3);
-    ColumnVector b = new ColumnVector(m);
-    for (int t = 0; t < m; t++){
-      A.set(t, 0, 1);
-      A.set(t, 1, t);
-      A.set(t, 2, t*t);
-      double perturbation = Math.random();
-      if (Math.random() < .5) perturbation *= -1;
-      b.set(t, (C+D*t+E*t*t + perturbation));
-    }
-    //System.out.println("A = " + A + "\n");
-    //System.out.println(b);
-
-    ColumnVector xhat = (A.transpose().multiply(A)).solve(A.transpose().multiply(b)).columnVector();
-    System.out.println("Original values vs. reconstructed values:\nC: " + C + " -> " + xhat.get(0)
-                       + "\nD: " + D + " -> " + xhat.get(1)
-                       + "\nE: " + E + " -> " + xhat.get(2));
+    System.out.println(A.inverseIteration());
   }
 }
